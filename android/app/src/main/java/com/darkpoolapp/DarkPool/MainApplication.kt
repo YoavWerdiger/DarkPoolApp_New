e0e8ac2811f8d7b48f277082bbe659c0944b09ce
DarkPool Dev <dev@darkpool.app>
2025-11-03 00:24:55 +0200

    Fix MainApplication.kt for React Native 0.79.5 compatibility

--- android/app/src/main/java/com/darkpoolapp/DarkPool/MainApplication.kt
+++ android/app/src/main/java/com/darkpoolapp/DarkPool/MainApplication.kt
@@ -5,11 +5,9 @@ import android.content.res.Configuration
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 
@@ -43,12 +41,7 @@ class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, /* native exopackage */ false)
-    DefaultNewArchitectureEntryPoint.releaseLevel = try {
-      ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
-    } catch (e: IllegalArgumentException) {
-      ReleaseLevel.STABLE
-    }
-    loadReactNative(this)
+    DefaultNewArchitectureEntryPoint.load()
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
